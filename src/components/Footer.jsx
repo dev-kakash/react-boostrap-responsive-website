92ef69ff0ea34ea67b14c8df171125b882f3eb0b
dev-kakash <mdatikasifkhanakash@gmail.com>
2021-07-04 12:42:35 +0600

    init

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <footer className="w-100 bg-dark  text-center">
-        <p>© 2021 Akash.All Rights Reserved | Terms and Conditions</p>
+        <p>© 2021 Akash.All Rights Reserved | Terms and Conditions :</p>
       </footer>
     </div>
   );
